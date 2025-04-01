8eccb4b2babf3dbdec9432237a4e9c6fd6be0c8b
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-04-01 14:21:52 -0400

    timer functional, disable start btn after game start

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const startBtn = document.getElementById('start-btn')
 const moleHoles = document.querySelectorAll('.hole')
+const timerEl = document.getElementById('timer')
+let timesUp = false
 
 function getRandomIndex() {
     index = Math.floor(Math.random() * 3) + 1
@@ -11,6 +13,28 @@ function getRandomTime(min, max) {
     return Math.round(Math.random() * (max - min + 1)) + min
 }
 
+function runTimer() {
+    let seconds = 59 
+    let timer = setInterval(() => {
+        if (seconds > 9) {
+            timerEl.innerText = `00:${seconds}`
+        } else if (seconds <= 9 ) {
+            timerEl.innerText = `00:0${seconds}`
+        } else  {
+            timerEl.innerText = '00:00'
+        }
+        
+        if (seconds === 0) {
+            clearInterval(timer)
+            timesUp = true
+            console.log('game over')
+        } else {
+            seconds--
+        }
+
+    }, 1000)
+
+}
 
 moleHoles.forEach(hole => {
     hole.addEventListener('click', () => {
@@ -21,8 +45,10 @@ moleHoles.forEach(hole => {
 
 function molePeek() {
     const currentMole = document.querySelector(`#hole-${index} .mole`)
+    // mole up
     currentMole.classList.remove('hidden')
-
+    
+    // mole down
     setTimeout(() => {
         currentMole.classList.add('hidden')
     }, getRandomTime(500, 1000));
@@ -30,6 +56,8 @@ function molePeek() {
 
 startBtn.addEventListener('click', () => {
     console.log('start game')
+    startBtn.disabled = true
     getRandomIndex()
+    runTimer()
     molePeek(index)
 })
