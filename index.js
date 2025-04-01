0ac6e544c3fb8fef1e12738ff37d5ecf6441fc9e
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-04-01 15:04:36 -0400

    add whack function

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const startBtn = document.getElementById('start-btn')
 const moleHoles = document.querySelectorAll('.hole')
 const timerEl = document.getElementById('timer')
+const scoreEl = document.getElementById('score')
 let timesUp = false
+let score = 0
 
 function getRandomIndex() {
     index = Math.floor(Math.random() * 3) + 1
-    console.log('random index:', index)
+    // console.log('random index:', index)
     return index
 }
 
 function getRandomTime(min, max) {
     return Math.round(Math.random() * (max - min + 1)) + min
 }
+
+// function getRandomHole() {
+//     const index = Math.floor(Math.random() * 3) + 1
+//     const hole = holes[index]
+//     return hole
+// }
 
 function runTimer() {
     let seconds = 59 
@@ -38,26 +46,40 @@ function runTimer() {
 
 moleHoles.forEach(hole => {
     hole.addEventListener('click', () => {
-        console.log(hole.id)
+        // console.log(hole.id)
+    if( hole.id === `hole-${index}` ){
+        score++
+        scoreEl.innerText = score
+        console.log('whack!', hole.id)
+    }
     })
 })
 
 
+startBtn.addEventListener('click', () => {
+    console.log('start game')
+    startBtn.disabled = true
+    runTimer()
+    getRandomIndex()
+    molePeek(index)
+})
+
 function molePeek() {
+    const time = getRandomTime(500, 1000)
     const currentMole = document.querySelector(`#hole-${index} .mole`)
     // mole up
     currentMole.classList.remove('hidden')
     
-    // mole down
+    // mole down & repeat
     setTimeout(() => {
         currentMole.classList.add('hidden')
-    }, getRandomTime(500, 1000));
+        if (!timesUp) {
+            getRandomIndex()
+            molePeek(index)
+        }
+    }, time);
 }
 
-startBtn.addEventListener('click', () => {
-    console.log('start game')
-    startBtn.disabled = true
-    getRandomIndex()
-    runTimer()
-    molePeek(index)
-})
+function whack() {
+
+}
